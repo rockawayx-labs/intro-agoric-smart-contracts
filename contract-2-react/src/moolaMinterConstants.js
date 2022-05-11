c3d3586bd53ef63a92b523910297b1e3cf27e614
Martin Vejmelka <vejmelkam@gmail.com>
2022-05-11 14:06:59 +0200

    initial commit of the React app

---
+++ contract-2-react/src/moolaMinterConstants.js
@@ -0,0 +1,5 @@
+// GENERATED FROM /Users/vejmelka/Projects/rbf/gateway-agoric-workshop-2022/contract-1/deploy.js
+export default {
+  "CONTRACT_NAME": "moolaMinter",
+  "INSTALLATION_BOARD_ID": "board05815"
+};

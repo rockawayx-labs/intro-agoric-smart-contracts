a808fdbf9fa7d7d73564b6e40725c636580a5a86
Martin Vejmelka <vejmelkam@gmail.com>
2022-05-11 18:02:54 +0200

    fixed initial errors

--- contract-2-react/src/App.js
+++ contract-2-react/src/App.js
@@ -1,6 +1,6 @@
 import '@agoric/eventual-send/shim';
 import { E } from '@agoric/eventual-send';
-import { AmountMath, makeIssuerKit } from '@agoric/ertp'
+import { AmountMath } from '@agoric/ertp'
 
 import React, { useCallback } from 'react';
 import { makeReactAgoricWalletConnection } from '@agoric/wallet-connection/react.js';
@@ -21,12 +21,10 @@ const MyWalletConnection = ({ connecting }) => {
     console.log('onWalletState', state);
     switch (state) {
       case 'idle': {
-        console.log('idle', ev.detail);
-
+        console.log('Connection with wallet established!')
         // This is one of the only methods that the wallet connection facet allows.
         // It connects asynchronously, but you can use promise pipelining immediately.
         const bridge = E(walletConnection).getScopedBridge('Contract-2');
-        console.log('idle!')
 
         // You should reconstruct all state here.
         const zoe = await E(bridge).getZoe();
@@ -37,16 +35,35 @@ const MyWalletConnection = ({ connecting }) => {
 
         const moolaInstance = await E(zoe).startInstance(moolaInstallation)
         const { creatorFacet: moolaCreatorFacet } = moolaInstance
-        const { getIssuer, makeMintSomeInvitation } = moolaCreatorFacet
 
-        const moolaIssuer = getIssuer()
-        console.log(moolaIssuer)
+        const moolaIssuer = await E(moolaCreatorFacet).getIssuer()
+        const moolaBrand = await E(moolaIssuer).getBrand()
 
-        const moola100 = AmountMath.make(moolaIssuer.getBrand(), 100n)
-        console.log(moola100)
+        const moola100 = AmountMath.make(moolaBrand, 100n)
+        const mintInvitation = await E(moolaCreatorFacet).makeMintSomeInvitation()
+
+        // FIXME: I don't seem to have to approve this offer in my wallet?
+        const moolaSeat = await E(zoe).offer(mintInvitation, { want: { Tokens: moola100 } })
+        const moolaResult = await E(moolaSeat).getOfferResult()
+        console.log(moolaResult)
+
+        const myMoola = await E(moolaSeat).getPayout('Tokens')
+        // FIXME: how to deposit this into a purse that is in my wallet?
+        console.log(myMoola)
 
         const nftInstance = await E(zoe).startInstance(nftInstallation, { Tokens: moolaIssuer }, { Moola: moolaIssuer })
-        console.log(nftInstance)
+        const { creatorFacet: nftCreatorFacet } = nftInstance
+
+        const nftIssuer = await E(nftCreatorFacet).getNFTIssuer()
+        const nftBrand = await E(nftIssuer).getBrand()
+        const nftMintInvitation = await E(nftCreatorFacet).mintNFTsInvitation()
+        console.log(nftMintInvitation)
+
+        const wantNFT = AmountMath.make(nftBrand, harden(["cryptopunk4551"]))
+        const nftSeat = await E(zoe).offer(nftMintInvitation, { want: { Awesomez: wantNFT }, give: { Tokens: moola100 } }, { Tokens: myMoola })
+
+        const nftResult = await E(nftSeat).getOfferResult()
+        console.log(nftResult)
 
         break;
       }
@@ -69,7 +86,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <MyWalletConnection />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -82,6 +98,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <MyWalletConnection />
     </div>
   );
 }

1e4e7b468c45c629e57fb663d54e7dc16d707e85
Martin Vejmelka <vejmelkam@gmail.com>
2022-05-12 20:53:45 +0200

    added token brand and issuer to board

--- react-ui/src/moolaMinterConstants.js
+++ react-ui/src/moolaMinterConstants.js
@@ -1,5 +1,7 @@
 // GENERATED FROM /Users/vejmelka/Projects/rbf/gateway-agoric-workshop-2022/contract-1/deploy.js
 export default {
   "CONTRACT_NAME": "moolaMinter",
-  "INSTALLATION_BOARD_ID": "board05815"
+  "INSTALLATION_BOARD_ID": "board02314",
+  "TOKEN_ISSUER_BOARD_ID": "board04312",
+  "TOKEN_BRAND_BOARD_ID": "board00613"
 };
--- react-ui/src/nftMinterConstants.js
+++ react-ui/src/nftMinterConstants.js
@@ -1,5 +1,5 @@
 // GENERATED FROM /Users/vejmelka/Projects/rbf/gateway-agoric-workshop-2022/contract-2/deploy.js
 export default {
   "CONTRACT_NAME": "nftMinter",
-  "INSTALLATION_BOARD_ID": "board04312"
+  "INSTALLATION_BOARD_ID": "board05815"
 };

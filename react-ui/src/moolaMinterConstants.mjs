721c771d7a8dd1ae0f2b6e76467f30ccc32ebaae
Martin Vejmelka <vejmelkam@gmail.com>
2022-05-17 11:55:48 +0200

    removing generated constants from git

--- react-ui/src/moolaMinterConstants.mjs
+++
@@ -1,9 +0,0 @@
-// GENERATED FROM /Users/vejmelka/Projects/rbf/gateway-agoric-workshop-2022/contract-1/deploy.js
-export default {
-  "CONTRACT_NAME": "moolaMinter",
-  "INSTALLATION_BOARD_ID": "board01422",
-  "INSTANCE_BOARD_ID": "board03523",
-  "TOKEN_ISSUER_BOARD_ID": "board06120",
-  "TOKEN_BRAND_BOARD_ID": "board02021",
-  "INVITE_BRAND_BOARD_ID": "board0223"
-};
--- react-ui/src/nftMinterConstants.js
+++
@@ -1,8 +0,0 @@
-// GENERATED FROM /Users/vejmelka/Projects/rbf/gateway-agoric-workshop-2022/contract-2/deploy.js
-export default {
-  "CONTRACT_NAME": "nftMinter",
-  "INSTALLATION_BOARD_ID": "board00126",
-  "INSTANCE_BOARD_ID": "board04827",
-  "NFT_ISSUER_BOARD_ID": "board05024",
-  "NFT_BRAND_BOARD_ID": "board03125"
-};
